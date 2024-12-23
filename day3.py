df9c27c61b917aa5de751820297426b7b810b688
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2024-12-23 16:31:51 -0300

    Part 2 completed

--- day3.py
+++ day3.py
@@ -1,15 +1,18 @@
 
-inpt='xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
-inpt=open('input_day3.txt')
+#text='xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))' #part1
+#text='xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))' #part2
+inpt=open('input_day3.txt',encoding='utf-8')
 
 d='mul('
 m=0
 text=''
 
+
 for line in inpt:
     text=text+line
 inpt.close()
 
+do=True
 for e in text.split(d)[1:]:
     s=d
     n=''
@@ -33,8 +36,20 @@ for e in text.split(d)[1:]:
                 m2=int(o)
                 break
         if e[j]==')':
-            s=s+e[j]
-            m=m+m1*m2
-            print(s)
+            if do==True:
+                s=s+e[j]
+                m=m+m1*m2
+                print(s)
+    ## check for the do/don't
+    for k in range(j,len(e)):
+        #print(k)
+        if e[k:k+2]=='do':
+            if e[k+2:k+4]=='()':
+                #print(e[k+2:k+4])
+                do=True
+                k=k+4
+            elif e[k+2:k+7]=='n\'t()':
+                do=False
+                k=k+7
 print(m)
             
